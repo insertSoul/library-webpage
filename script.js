7b8bd657be11405b82238062fba95e87dff05728
insertSoul <j.clarke213@gmail.com>
2023-08-15 12:36:34 +0100

    Setup of inital script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('test')
+const myLibrary = []
+
+function Book(title, author, pages, readStatus){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readStatus = readStatus
+    this.info = function(){
+        if (this.readStatus === true) {
+            return(`The ${this.title} by ${this.author} is ${this.pages} pages long; has been read`);
+        } else {
+            return(`The ${this.title} by ${this.author} is ${this.pages} pages long; has not been read yet`);
+        }
+    };
+};
+
+function addBookToLibrary() {
+    //to build
+}
+
